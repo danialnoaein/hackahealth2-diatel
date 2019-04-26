4adda4e215e038bbdb921f69caff6761ec754810
Danial Noaein <rbkk.noaein@gmail.com>
2019-04-26 13:42:29 +0430

    Add essential activities
    AddReminderActivity
    ArticlesActivity
    FirstVisitActivity
    ImmediateCheckupActivity
    SplashActivity

---
+++ app/src/main/java/hackahealth/diatel/ImmediateCheckupActivity.java
@@ -0,0 +1,13 @@
+package hackahealth.diatel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImmediateCheckupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_immediate_checkup);
+    }
+}
