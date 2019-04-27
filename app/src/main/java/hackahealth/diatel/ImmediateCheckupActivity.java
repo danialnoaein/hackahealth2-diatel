8b2126271df852733231746963dd5da6a1516e54
Danial Noaein <rbkk.noaein@gmail.com>
2019-04-27 13:07:47 +0430

    Build Project and activities
    Build Project
    Add main acticities:
    AddReminderActivity
    ArticlesActivity
    FirstVisitActivity
    ImmediateCheckupActivity
    MainActivity
    SplashActivity

--- app/src/main/java/hackahealth/diatel/ImmediateCheckupActivity.java
+++ app/src/main/java/hackahealth/diatel/ImmediateCheckupActivity.java
@@ -9,5 +9,6 @@ public class ImmediateCheckupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_immediate_checkup);
+
     }
 }
