36d2c5aa66b3930f5efc331dabb95a5f24dd86cd
Danial Noaein <rbkk.noaein@gmail.com>
2019-04-27 14:58:15 +0430

    Complete SplashActivity

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -75,18 +75,18 @@ public final class R {
 
         public static final int bottom = 0x7f080031;
         public static final int end = 0x7f08004d;
-        public static final int gone = 0x7f08005b;
-        public static final int invisible = 0x7f080066;
-        public static final int left = 0x7f08006b;
-        public static final int packed = 0x7f080082;
-        public static final int parent = 0x7f080084;
-        public static final int percent = 0x7f080087;
-        public static final int right = 0x7f08008c;
-        public static final int spread = 0x7f0800b0;
-        public static final int spread_inside = 0x7f0800b1;
-        public static final int start = 0x7f0800b6;
-        public static final int top = 0x7f0800cb;
-        public static final int wrap = 0x7f0800dc;
+        public static final int gone = 0x7f08005d;
+        public static final int invisible = 0x7f080068;
+        public static final int left = 0x7f08006d;
+        public static final int packed = 0x7f080084;
+        public static final int parent = 0x7f080086;
+        public static final int percent = 0x7f08008a;
+        public static final int right = 0x7f08008f;
+        public static final int spread = 0x7f0800b3;
+        public static final int spread_inside = 0x7f0800b4;
+        public static final int start = 0x7f0800b9;
+        public static final int top = 0x7f0800ce;
+        public static final int wrap = 0x7f0800df;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/hackahealth/diatel/SplashActivity.java
+++ app/src/main/java/hackahealth/diatel/SplashActivity.java
@@ -1,13 +1,73 @@
 package hackahealth.diatel;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.github.ybq.android.spinkit.SpinKitView;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Context context = this;
+    ExpandableLayout exp_diatel, exp_magical;
+    SpinKitView pb_loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        findViews();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                exp_diatel.expand();
+            }
+        }, 1000);
+
+        exp_diatel.setOnExpansionUpdateListener(new ExpandableLayout.OnExpansionUpdateListener() {
+            @Override
+            public void onExpansionUpdate(float expansionFraction, int state) {
+                if (state == ExpandableLayout.State.EXPANDED) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            exp_magical.expand();
+                        }
+                    }, 500);
+                }
+            }
+        });
+        exp_magical.setOnExpansionUpdateListener(new ExpandableLayout.OnExpansionUpdateListener() {
+            @Override
+            public void onExpansionUpdate(float expansionFraction, final int state) {
+                if (state == ExpandableLayout.State.EXPANDED) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            startActivity(new Intent(getContext(), MainActivity.class));
+                            finish();
+                        }
+                    }, 1000);
+
+                }
+            }
+        });
+    }
+
+    private void findViews() {
+        exp_diatel = findViewById(R.id.exp_diatel);
+        exp_magical = findViewById(R.id.exp_magical);
+        pb_loading = findViewById(R.id.pb_loading);
+    }
+
+    public Context getContext() {
+        return context;
     }
 }
