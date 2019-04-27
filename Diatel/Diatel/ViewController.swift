0f45bca32c8d18e9207a0351949338a7beb411c7
mohammed reza <mohammed.r.ghate@gmail.com>
2019-04-27 11:28:30 +0430

    Project have Created

---
+++ Diatel/Diatel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diatel
+//
+//  Created by M.A.R.G on 2/7/1398 AP.
+//  Copyright © 1398 MohamedRezaGhate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
