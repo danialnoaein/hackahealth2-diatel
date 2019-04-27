c2aa384099f3c294e5b65eeb5c2891fa187bb969
mohammed reza <mohammed.r.ghate@gmail.com>
2019-04-27 19:14:48 +0430

    Collectionview for Trainig and Custom Collectionviewcell created

--- Diatel/Diatel/Controller/Traning/TrainigViewController.swift
+++ Diatel/Diatel/Controller/Traning/TrainigViewController.swift
@@ -13,13 +13,14 @@ class TrainigViewController: UIViewController,UICollectionViewDelegate,UICollect
     
 
     @IBOutlet weak var collectionview: UICollectionView!
-    let titles = ["تغذیه و رژیم درمانی","علائم بالینی","دارو خانه" ,"بیماری های مرتبط","مصاحبه پزشکان","پیشگیری"];
-    let colors = [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1),#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1),#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1),#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1),#colorLiteral(red: 0.1254901961, green: 0.3882352941, blue: 0.6078431373, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)]
+    let titles = ["درمان و پیشگیری","تغذیه و رژیم","علائم بالینی","دارو خانه" ,"بیماری های مرتبط","مصاحبه پزشکان"];
+
     override func viewDidLoad() {
         super.viewDidLoad();
         collectionview.delegate = self
         collectionview.dataSource = self
         // Do any additional setup after loading the view.
+        collectionview.register(UINib.init(nibName: "TraningCell", bundle: nil), forCellWithReuseIdentifier: "sectioncell")
     }
     
     
@@ -28,17 +29,19 @@ class TrainigViewController: UIViewController,UICollectionViewDelegate,UICollect
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sectioncell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sectioncell", for: indexPath) as! TraningCell
         
         cell.layer.cornerRadius = 10
-        cell.backgroundColor = colors[indexPath.row]
-        
+        let color1 = UIColor(red: 0.2352941176, green: 0.6823529412, blue: 0.6392156863, alpha: 1)
+        let color2 = UIColor(red: 0.1254901961, green: 0.3882352941, blue: 0.6078431373, alpha: 1)
+        cell.setGradientBackground(colorTop: color1, colorBottom: color2)
+        cell.titlebtn.setTitle(titles[indexPath.row], for: .normal)
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 130, height: 130)
+        return CGSize(width: ((collectionView.frame.width / 2) - 10)  , height: ((collectionView.frame.height / 3) - 10))
     }
     
 
---
+++ Diatel/Diatel/View/customcell/TraningCell.swift
@@ -0,0 +1,19 @@
+//
+//  TraningCell.swift
+//  Diatel
+//
+//  Created by M.A.R.G on 2/7/1398 AP.
+//  Copyright © 1398 MohamedRezaGhate. All rights reserved.
+//
+
+import UIKit
+
+class TraningCell: UICollectionViewCell {
+
+    @IBOutlet weak var titlebtn: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
