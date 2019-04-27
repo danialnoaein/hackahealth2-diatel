f0858c813042dcb39d94e059e3fdf0fe592dc207
mohammed reza <mohammed.r.ghate@gmail.com>
2019-04-27 19:53:40 +0430

    DetailViewcontroller created for Traning CollectionView Cells

---
+++ Diatel/Diatel/Controller/Traning/DetailTraning.swift
@@ -0,0 +1,22 @@
+//
+//  DetailTraning.swift
+//  Diatel
+//
+//  Created by M.A.R.G on 2/7/1398 AP.
+//  Copyright © 1398 MohamedRezaGhate. All rights reserved.
+//
+
+import UIKit
+
+class DetailTraning: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
---
+++ Diatel/Diatel/View/customcell/TrainingTablecell.swift
@@ -0,0 +1,28 @@
+//
+//  TrainingTablecell.swift
+//  Diatel
+//
+//  Created by M.A.R.G on 2/7/1398 AP.
+//  Copyright © 1398 MohamedRezaGhate. All rights reserved.
+//
+
+import UIKit
+
+class TrainingTablecell: UITableViewCell {
+
+    @IBOutlet weak var container: UIView!
+    @IBOutlet weak var imagePlaceholder: UIImageView!
+    @IBOutlet weak var titleText: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        container.layer.cornerRadius = 10
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
