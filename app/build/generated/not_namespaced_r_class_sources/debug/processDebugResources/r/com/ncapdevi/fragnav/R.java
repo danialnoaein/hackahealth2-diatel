f0b08080ec54ae0a18d10da4de76a161fdecf7b2
Danial Noaein <rbkk.noaein@gmail.com>
2019-04-29 02:07:49 +0430

    Add day change in Regime fragment

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/ncapdevi/fragnav/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/ncapdevi/fragnav/R.java
@@ -62,18 +62,18 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int notification_action_background = 0x7f070095;
-        public static final int notification_bg = 0x7f070096;
-        public static final int notification_bg_low = 0x7f070097;
-        public static final int notification_bg_low_normal = 0x7f070098;
-        public static final int notification_bg_low_pressed = 0x7f070099;
-        public static final int notification_bg_normal = 0x7f07009a;
-        public static final int notification_bg_normal_pressed = 0x7f07009b;
-        public static final int notification_icon_background = 0x7f07009c;
-        public static final int notification_template_icon_bg = 0x7f07009d;
-        public static final int notification_template_icon_low_bg = 0x7f07009e;
-        public static final int notification_tile_bg = 0x7f07009f;
-        public static final int notify_panel_notification_icon_bg = 0x7f0700a0;
+        public static final int notification_action_background = 0x7f070096;
+        public static final int notification_bg = 0x7f070097;
+        public static final int notification_bg_low = 0x7f070098;
+        public static final int notification_bg_low_normal = 0x7f070099;
+        public static final int notification_bg_low_pressed = 0x7f07009a;
+        public static final int notification_bg_normal = 0x7f07009b;
+        public static final int notification_bg_normal_pressed = 0x7f07009c;
+        public static final int notification_icon_background = 0x7f07009d;
+        public static final int notification_template_icon_bg = 0x7f07009e;
+        public static final int notification_template_icon_low_bg = 0x7f07009f;
+        public static final int notification_tile_bg = 0x7f0700a0;
+        public static final int notify_panel_notification_icon_bg = 0x7f0700a1;
     }
     public static final class id {
         private id() {}
@@ -85,25 +85,25 @@ public final class R {
         public static final int actions = 0x7f080026;
         public static final int async = 0x7f08002c;
         public static final int blocking = 0x7f080030;
-        public static final int chronometer = 0x7f080043;
-        public static final int forever = 0x7f08006e;
-        public static final int icon = 0x7f080075;
-        public static final int icon_group = 0x7f080076;
-        public static final int info = 0x7f08007c;
-        public static final int italic = 0x7f08007e;
-        public static final int line1 = 0x7f080083;
-        public static final int line3 = 0x7f080084;
-        public static final int normal = 0x7f08009b;
-        public static final int notification_background = 0x7f08009c;
-        public static final int notification_main_column = 0x7f08009d;
-        public static final int notification_main_column_container = 0x7f08009e;
-        public static final int right_icon = 0x7f0800ba;
-        public static final int right_side = 0x7f0800bb;
-        public static final int tag_transition_group = 0x7f0800e9;
-        public static final int text = 0x7f0800ec;
-        public static final int text2 = 0x7f0800ed;
-        public static final int time = 0x7f0800f5;
-        public static final int title = 0x7f0800f6;
+        public static final int chronometer = 0x7f080044;
+        public static final int forever = 0x7f08006f;
+        public static final int icon = 0x7f080076;
+        public static final int icon_group = 0x7f080077;
+        public static final int info = 0x7f08007d;
+        public static final int italic = 0x7f08007f;
+        public static final int line1 = 0x7f080084;
+        public static final int line3 = 0x7f080085;
+        public static final int normal = 0x7f08009c;
+        public static final int notification_background = 0x7f08009d;
+        public static final int notification_main_column = 0x7f08009e;
+        public static final int notification_main_column_container = 0x7f08009f;
+        public static final int right_icon = 0x7f0800bc;
+        public static final int right_side = 0x7f0800bd;
+        public static final int tag_transition_group = 0x7f0800eb;
+        public static final int text = 0x7f0800ee;
+        public static final int text2 = 0x7f0800ef;
+        public static final int time = 0x7f0800f7;
+        public static final int title = 0x7f0800f8;
     }
     public static final class integer {
         private integer() {}
@@ -113,12 +113,12 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int notification_action = 0x7f0b0047;
-        public static final int notification_action_tombstone = 0x7f0b0048;
-        public static final int notification_template_custom_big = 0x7f0b0049;
-        public static final int notification_template_icon_group = 0x7f0b004a;
-        public static final int notification_template_part_chronometer = 0x7f0b004b;
-        public static final int notification_template_part_time = 0x7f0b004c;
+        public static final int notification_action = 0x7f0b0048;
+        public static final int notification_action_tombstone = 0x7f0b0049;
+        public static final int notification_template_custom_big = 0x7f0b004a;
+        public static final int notification_template_icon_group = 0x7f0b004b;
+        public static final int notification_template_part_chronometer = 0x7f0b004c;
+        public static final int notification_template_part_time = 0x7f0b004d;
     }
     public static final class string {
         private string() {}
--- app/src/main/java/fragments/main_fragments/RegimeFragment.java
+++ app/src/main/java/fragments/main_fragments/RegimeFragment.java
@@ -1,5 +1,6 @@
 package fragments.main_fragments;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,6 +12,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.VideoView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -20,9 +23,11 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.ybq.android.spinkit.SpinKitView;
 
 import java.util.ArrayList;
 
+import danialnoaein_widgets.Button;
 import danialnoaein_widgets.TextView;
 import hackahealth.diatel.GlucometerGuideActivity;
 import hackahealth.diatel.ImmediateCheckupActivity;
@@ -30,6 +35,7 @@ import hackahealth.diatel.R;
 
 public class RegimeFragment extends Fragment {
 
+    Button btn_change_week_day;
     public static RegimeFragment newInstance() {
         Bundle args = new Bundle();
         RegimeFragment fragment = new RegimeFragment();
@@ -47,7 +53,7 @@ public class RegimeFragment extends Fragment {
     }
 
     private void findViews(View fragmentView) {
-
+        btn_change_week_day = fragmentView.findViewById(R.id.btn_change_week_day);
     }
 
     @Override
@@ -57,10 +63,31 @@ public class RegimeFragment extends Fragment {
     }
 
     private void onClick(){
+        btn_change_week_day.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+                LayoutInflater inflater = requireActivity().getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.dialog_days_of_week, null);
+                builder.setView(dialogView);
+                final AlertDialog dialog = builder.create();
+                dialog.show();
+
+                RadioGroup rg_days_of_week = dialogView.findViewById(R.id.rg_days_of_week);
+                rg_days_of_week.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+
+
+            }
+        });
     }
-
-
 
 }
 
